1d151239af1eb549db016a5aab2b0843116723ae
ikeemthedream <55724332+ikeemthedream@users.noreply.github.com>
2023-09-24 14:51:14 -0400

    print status of players

--- GameManager.cs
+++ GameManager.cs
@@ -13,8 +13,8 @@ namespace PlayersStory
         {
             Console.WriteLine("Enter a Name: ");
             User.Name = Console.ReadLine();
-            Console.WriteLine($"Hi, {User.Name}\nYour Opponent is:\n{opponent.Name}");
-            StopPhase();
+            Console.WriteLine($"\nHi, {User.Name}\nYour Opponent is:\n{opponent.Name}");
+            PausePhase();
         }
         public static void Game(Player User, Player opponent)
         {
@@ -28,9 +28,9 @@ namespace PlayersStory
             #endregion
             while (true)
             {
-                PhaseOne(User, opponent, out targetNumber, out userNumber, out opponentNumber, out result1, out result2);
-                PhaseTwo(User, opponent, result1, result2);
-                #region EndGame
+                GettingUserInput(User, opponent, out targetNumber, out userNumber, out opponentNumber);
+                CalculateResult(targetNumber, userNumber, opponentNumber, out result1, out result2);
+                ComparResults(User, opponent, result1, result2);
                 if (User.Health > 0 && opponent.Health > 0)
                 {
                     continue;
@@ -40,35 +40,34 @@ namespace PlayersStory
                     Console.WriteLine("Game Over");
                     return;
                 }
-
-                return;
-                #endregion
             }
         }
-        private static void PhaseOne(Player User, Player opponent, out int targetNumber, out int userNumber, out int opponentNumber, out int result1, out int result2)
+        private static void GettingUserInput(Player User, Player opponent, out int targetNumber, out int userNumber, out int opponentNumber)
         {
+            ShowStatus(User, opponent);
             #region Host Pick
             Console.WriteLine("Ask a friend to select a random " +
             "target number betweem 1 and 100");
             targetNumber = Convert.ToInt32(Console.ReadLine());
-            StopPhase();
+            PausePhase();
             #endregion
             #region Player Pick
             Console.WriteLine($"{User.Name}, guess what the target number is: ");
             userNumber = Convert.ToInt32(Console.ReadLine());
-            StopPhase();
+            PausePhase();
             #endregion
             #region Opponent Pick
             Console.WriteLine($"{opponent.Name}, guess what the target number is: ");
             opponentNumber = Convert.ToInt32(Console.ReadLine());
-            StopPhase();
-            #endregion
-            #region Results
-            result1 = targetNumber - userNumber;
-            result2 = targetNumber - opponentNumber;
+            PausePhase();
             #endregion
         }
-        private static void PhaseTwo(Player User, Player opponent, int result1, int result2)
+        private static void CalculateResult(int targetNumber, int userNumber, int opponentNumber, out int result1, out int result2)
+        {
+            result1 = targetNumber - userNumber;
+            result2 = targetNumber - opponentNumber;
+        }
+        private static void ComparResults(Player User, Player opponent, int result1, int result2)
         {
             #region Condition
             if (result1 < result2)
@@ -81,18 +80,25 @@ namespace PlayersStory
                 Console.WriteLine("Ikeem Wins\n");
                 Attack(opponent, User);
             }
+            ShowStatus(User, opponent);
             Console.ReadKey();
             Console.Clear();
             #endregion
         }
-        private static void StopPhase()
+        private static void PausePhase()
         {
+            Console.WriteLine($"Press any key to continue...");
             Console.ReadKey();
             Console.Clear();
         }
         public static void Attack(Player Winner, Player Loser)
         {
             Loser.Health -= Winner.AttkPwr;
+        }
+        private static void ShowStatus(Player User, Player opponent)
+        {
+            UIManager.PrintStatus(User);
+            UIManager.PrintStatus(opponent);
         }
     }
 }
